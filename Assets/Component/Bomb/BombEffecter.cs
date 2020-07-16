e64b23d8a0016b1eecf6cba386b4fc8ea05481c5
luckynwj7 <44963879+luckynwj7@users.noreply.github.com>
2020-07-17 00:27:44 +0900

    [폭탄 터지는 이펙트 구현]
    폭탄 터지는 이펙트를 구현했습니다.

---
+++ Assets/Component/Bomb/BombEffecter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombEffecter : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        explosionEffect.transform.position = transform.position;
+        Instantiate(explosionEffect);
+        Destroy(gameObject); //TODO: 이펙트라 자동삭제되지만, 안전하게 삭제하는 방법이 필요함?
+    }
+}
